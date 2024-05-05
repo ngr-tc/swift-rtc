5754f0dcbfd4d14bd457efe9e936e34c744b3d00
yngrtc <y@ngr.tc>
2024-05-04 21:22:38 -0700

    copyright and contributors

--- Sources/rtc/rtc.swift
+++ Sources/rtc/rtc.swift
@@ -1,2 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftRTC open source project
+//
+// Copyright (c) 2024 ngRTC and the SwiftRTC project authors
+// Licensed under MIT License
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftRTC project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+@_exported import sdp
