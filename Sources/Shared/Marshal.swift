f7836f16e449c55d920a0f4f13f3ab1a49b69695
yngrtc <y@ngr.tc>
2024-06-08 08:24:05 -0700

    add Marshal

---
+++ Sources/Shared/Marshal.swift
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftRTC open source project
+//
+// Copyright (c) 2024 ngRTC and the SwiftRTC project authors
+// Licensed under MIT License
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftRTC project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+import NIOCore
+
+public protocol MarshalSize {
+    func marshalSize() -> Int
+}
+
+public protocol Marshal: MarshalSize {
+    func marshal(buf: inout ByteBuffer) throws -> Int
+}
+
+public protocol Unmarshal: MarshalSize {
+    init(buf: ByteBufferView) throws
+}
