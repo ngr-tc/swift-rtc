58db982dbfbe224ee7a972dcbf5c6c16473f6b80
yngrtc <y@ngr.tc>
2024-06-09 08:37:36 -0700

    add AudioLevelExtensionTests

--- Tests/RTPTests/AudioLevelExtensionTests.swift
+++ Tests/RTPTests/AudioLevelExtensionTests.swift
@@ -1,8 +1,66 @@
+import NIOCore
+//===----------------------------------------------------------------------===//
 //
-//  File.swift
-//  
+// This source file is part of the SwiftRTC open source project
 //
-//  Created by Yu Liu on 6/9/24.
+// Copyright (c) 2024 ngRTC and the SwiftRTC project authors
+// Licensed under MIT License
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftRTC project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+import XCTest
 
-import Foundation
+@testable import RTP
+
+final class AudioLevelExtensionTests: XCTestCase {
+    func testAudioLevelExtensionTooSmall() throws {
+        var buf = ByteBuffer()
+        let result = try? AudioLevelExtension(&buf)
+        XCTAssertTrue(result == nil)
+    }
+
+    func testAudioLevelExtensionVoiceTrue() throws {
+        let raw = ByteBuffer(bytes: [0x88])
+        var buf = raw.slice()
+        let a1 = try AudioLevelExtension(&buf)
+        let a2 = AudioLevelExtension(
+            level: 8,
+            voice: true
+        )
+        XCTAssertEqual(a1, a2)
+
+        var dst = ByteBuffer()
+        let _ = try a2.marshalTo(&dst)
+        XCTAssertEqual(raw, dst)
+    }
+
+    func testAudioLevelExtensionVoiceFalse() throws {
+        let raw = ByteBuffer(bytes: [0x8])
+        var buf = raw.slice()
+        let a1 = try AudioLevelExtension(&buf)
+        let a2 = AudioLevelExtension(
+            level: 8,
+            voice: false
+        )
+        XCTAssertEqual(a1, a2)
+
+        var dst = ByteBuffer()
+        let _ = try a2.marshalTo(&dst)
+        XCTAssertEqual(raw, dst)
+    }
+
+    func testAudioLevelExtensionLevelOverflow() throws {
+        let a = AudioLevelExtension(
+            level: 128,
+            voice: false
+        )
+
+        var dst = ByteBuffer()
+        let result = try? a.marshalTo(&dst)
+        XCTAssertTrue(result == nil)
+    }
+}
