b1156671bd17179f52ac09769bafa2c59016f275
yngrtc <y@ngr.tc>
2024-06-09 08:42:26 -0700

    add TransportCcExtension

--- Sources/RTP/Extension/TransportCcExtension.swift
+++ Sources/RTP/Extension/TransportCcExtension.swift
@@ -13,3 +13,51 @@
 //===----------------------------------------------------------------------===//
 import NIOCore
 import Shared
+
+// transport-wide sequence
+public let transportCcExtensionSize: Int = 2
+
+/// TransportCCExtension is a extension payload format in
+/// https://tools.ietf.org/html/draft-holmer-rmcat-transport-wide-cc-extensions-01
+/// 0                   1                   2                   3
+/// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |       0xBE    |    0xDE       |           length=1            |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |  ID   | L=1   |transport-wide sequence number | zero padding  |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+public struct TransportCcExtension: Equatable {
+    public var transportSequence: UInt16
+}
+
+extension TransportCcExtension: Unmarshal {
+    /// Unmarshal parses the passed byte slice and stores the result in the members
+    public init(_ buf: inout ByteBuffer) throws {
+        if buf.readableBytes < transportCcExtensionSize {
+            throw RtpError.errBufferTooSmall
+        }
+        guard let b0: UInt8 = buf.readInteger() else {
+            throw RtpError.errBufferTooSmall
+        }
+        guard let b1: UInt8 = buf.readInteger() else {
+            throw RtpError.errBufferTooSmall
+        }
+
+        self.transportSequence = (UInt16(b0) << 8) | UInt16(b1)
+    }
+}
+
+extension TransportCcExtension: MarshalSize {
+    /// MarshalSize returns the size of the TransportCcExtension once marshaled.
+    public func marshalSize() -> Int {
+        return transportCcExtensionSize
+    }
+}
+
+extension TransportCcExtension: Marshal {
+    /// Marshal serializes the members to buffer
+    public func marshalTo(_ buf: inout ByteBuffer) throws -> Int {
+        buf.writeInteger(self.transportSequence)
+        return transportCcExtensionSize
+    }
+}
---
+++ Tests/RTPTests/TransportCcExtensionTests.swift
@@ -0,0 +1,50 @@
+import NIOCore
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftRTC open source project
+//
+// Copyright (c) 2024 ngRTC and the SwiftRTC project authors
+// Licensed under MIT License
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftRTC project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+import XCTest
+
+@testable import RTP
+
+final class TransportCcExtensionTests: XCTestCase {
+    func test_transport_cc_extension_too_small() throws {
+        var buf = ByteBuffer()
+        let result = try? TransportCcExtension(&buf)
+        XCTAssertTrue(result == nil)
+    }
+
+    func test_transport_cc_extension() throws {
+        let raw = ByteBuffer(bytes: [0x00, 0x02])
+        var buf = raw.slice()
+        let t1 = try TransportCcExtension(&buf)
+        let t2 = TransportCcExtension(
+            transportSequence: 2
+        )
+        XCTAssertEqual(t1, t2)
+
+        var dst = ByteBuffer()
+        let _ = try t2.marshalTo(&dst)
+        XCTAssertEqual(raw, dst)
+    }
+
+    func test_transport_cc_extension_extra_bytes() throws {
+        let raw = ByteBuffer(bytes: [0x00, 0x02, 0x00, 0xff, 0xff])
+        var buf = raw.slice()
+        let t1 = try TransportCcExtension(&buf)
+        let t2 = TransportCcExtension(
+            transportSequence: 2
+        )
+        XCTAssertEqual(t1, t2)
+    }
+
+}
